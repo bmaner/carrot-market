546a44c3ff77f9ed029e8cbf8ea8edc479e143e4
bmaner <bmanerdaniel@gmail.com>
2022-02-26 13:00:04 +0900

    add components with tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,80 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return null;
+  // tailwind에서는 숫자 10이 2.5rem(40px)을 의미한다.
+  // 그러므로 4가 1rem 16px이겠구만
+  // flex flex-col space-y-5 대신에 grid gap-10을 넣을 수도 있다. 
+  return <div className="bg-slate-400 py-20 px-10 flex flex-col space-y-5 min-h-screen">
+    <div className="bg-white p-6 rounded-3xl shadow-xl">
+      <span className="font-semibold text-3xl">Select Item</span>
+      <div className="flex justify-between my-2" >
+        <span className="text-gray-500">Grey Chair</span>
+        <span className="font-semibold">$19</span>
+      </div>
+      <div className="flex justify-between">
+        <span className="text-gray-500">Tooly Table</span>
+        <span className="font-semibold">$19</span>
+      </div>
+      <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+        <span>Total</span>
+        <span className="font-semibold">$10</span>
+      </div>
+      <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto block hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">Checkout</button>
+    </div>
+    <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
+      <div className="bg-blue-500 p-6 pb-14">
+        <span className="text-white text-2xl">Profile</span>
+      </div>
+      <div className="rounded-3xl p-6 relative -top-5 bg-white">
+        <div className="flex relative -top-16 justify-between items-end">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500 ">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 bg-zinc-200 rounded-full"/>
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500 ">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
+        </div>
+        <div className="relative flex flex-col items-center -mt-10 -mb-5">
+          <span className="text-lg font-medium">Tony Molloy</span>
+          <span className="text-sm text-gray-500">미국</span>
+        </div>
+      </div>
+    </div>
+    <div className="bg-white p-6 rounded-3xl shadow-xl">
+      <div className="flex mb-5 justify-between items-center">
+        <span>⬅️</span>
+        <div className="space-x-3">
+          <span>🌟 4.9</span>
+          <span className="shadow-xl p-2 rounded-md">❤️</span>
+        </div>
+      </div>
+      <div className="bg-zinc-200 h-72 mb-5"/>
+      <div className="flex flex-col">
+          <span className="font-medium text-xl">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+        <div className="mt-3 mb-5 flex justify-between items-center">
+          <div className="space-x-2"> 
+            <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 ring-opacity-30 transition"></button>
+            <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 ring-opacity-30 transition"></button>
+            <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 ring-opacity-30 transition"></button>
+          </div>
+          <div className="flex items-center space-x-5 ">
+            <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">-</button>
+            <span>1</span>
+            <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">+</button>
+          </div>500
+        </div>
+        <div className="flex justify-between items-center">
+          <span className="font-medium text-2xl">$450</span>
+          <button className="py-2 px-8  bg-blue-500 text-xs text-center text-white rounded-lg ">Add to cart</button>
+        </div>
+      </div>
+    </div>
+    <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+  </div>
 }
 
 export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   content: [
-    './pages/**/*.{js, jsx, ts, tsx}',
-    './components/**/*.{js, jsx, ts, tsx}',
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
   ],
   //tailwind가 pages,components 폴더의 모든 디렉토리, .js, .jsx, .ts, .tsx로 끝나는 모든 파일에서 사용자가 tailwind를 사용한 것을 찾아낼 수 있다.
+  //tailwind.config.js를 설정 시 js, jsx, ts, tsx 이런식으로 띄어쓰면 적용이 안되니 주의!
   theme: {
     extend: {},
   },
