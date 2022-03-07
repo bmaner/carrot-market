a4da6cda554f58633245985a50ce93ca39d56a52
bmaner <bmanerdaniel@gmail.com>
2022-03-07 13:05:18 +0900

    Add layout component

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,25 +1,26 @@
 import type { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const Chats: NextPage = () => {
   return (
-    <div className="py-10 divide-y-[1px]">
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_, i) => {
-        return (
+    <Layout hasTabBar title="채팅">
+      <div className="divide-y-[1px] ">
+        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
           <div
             key={i}
-            className="px-4 flex items-center space-x-3 py-3 cursor-pointer last:border-b-0"
+            className="flex px-4 cursor-pointer py-3 items-center space-x-3"
           >
             <div className="w-12 h-12 rounded-full bg-slate-300" />
             <div>
               <p className="text-gray-700">Steve Jebs</p>
-              <p className="text-sm text-gray-500">
+              <p className="text-sm  text-gray-500">
                 See you tomorrow in the corner at 2pm!
               </p>
             </div>
           </div>
-        );
-      })}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
