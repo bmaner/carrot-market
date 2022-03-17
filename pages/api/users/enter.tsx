77611c088b59b0ae1bdf502624ce0326a4826afe
bmaner <bmanerdaniel@gmail.com>
2022-03-17 16:31:54 +0900

    Add upsert(findOrCreate) logic in accounts

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,21 +3,62 @@ import withHandler from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // if (req.method !== "POST") {
-  //   res.status(401).end();
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+      // ...(phone && { phone: +phone }),
+      // ...(email && { email: email }),
+      // ...(phone ? { phone: +phone } : {}), 효과는 위와 같음
+      // ...(email ? { email: email } : {}), 효과는 위와 같음
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+      // ...(phone && { phone: +phone }),
+      // ...(email && { email: email }),
+    },
+    update: {},
+  });
+  console.log(user);
+  // if (email) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email: email,
+  //     },
+  //   });
+  //   if (user) console.log("찾았다");
+  //   if (!user) {
+  //     console.log("못찾겠음");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         email: email,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
   // }
-  console.log("req.body?", req.body);
+  // if (phone) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //   });
+  //   if (user) console.log("찾았다");
+  //   if (!user) {
+  //     console.log("못찾겠음");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         phone: +phone,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
   return res.status(200).end();
 }
-
-//   await client.user.create({
-//     data: {
-//       email: "hiphop9308@naver.com",
-//       name: "paul",
-//     },
-//   });
-//   res.json({
-//     ok: true,
-//   });
 
 export default withHandler("POST", handler);
