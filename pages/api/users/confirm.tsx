fbb3f9bf653cc82abd92f9c10e27c8e79d450fea
bmaner <bmanerdaniel@gmail.com>
2022-03-21 19:38:31 +0900

    Add iron-session for authentication and authentication logic

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,14 +1,31 @@
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import { withIronSessionApiRoute } from "iron-session/next";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
+  // console.log(req.session);
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+    include: { user: true },
+  });
+  if (!exists) res.status(404).end();
+  console.log(exists);
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password:
+    "09187209387059817230984710234asdfasdahsqwtqweyqwehasdfasdfasdfasldkjgaopsgijjpqwoe;kab;lkzxcmbk",
+});
